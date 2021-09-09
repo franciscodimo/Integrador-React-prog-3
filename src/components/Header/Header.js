edfa7f99d5443c51454b8c0730940a41240f2cdb
Tomhenschien <thenschien@udesa.edu.ar>
2021-09-09 11:28:52 -0300

    botonRow orientación

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,6 +11,19 @@ class Header extends Component{
             botonRow: false,
              }
         }
+
+        botonRow(){
+            if(this.state.botonRow){
+                this.setState({
+                    botonRow: false,
+                })
+            }else{
+                this.setState({
+                    botonRow: true,
+                })
+            }
+        }
+
     evitarSubmit(e){
         e.preventDefault();
         console.log('Evitando el envío')
@@ -30,8 +43,8 @@ class Header extends Component{
             <section>
                 <h1 className="nombreApp">Título/ Nombre de la app</h1>
                 <p className="ordenar">Ordenar ASC/ DESC</p>
-                <i className="fas fa-align-justify"></i>
-                <i className="fas fa-th"></i>
+                <i className="fas fa-align-justify"onClick={()=>this.botonRow()}></i>
+                <i className="fas fa-th"onClick={()=>this.botonRow()}></i>
                 <form action="Buscar por Nombre" onSubmit={(milanesa)=>this.evitarSubmit(milanesa)}>
                 <input type="text" onChange={(papas)=>this.controlarCambios(papas)} value={this.state.filterBy} placehoder='ingrese su nombre'/>
                 </form>
