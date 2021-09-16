2f7963b33081d55161123ab41df085c0ef9f549b
franciscodimo <fdimo@udesa.edu.ar>
2021-09-16 20:29:57 -0300

    Sort

--- src/components/Artistas/Artistas.js
+++ src/components/Artistas/Artistas.js
@@ -9,8 +9,9 @@ class Artistas extends Component{
         this.state={
          artistas: [],
          proximaUrl: 11,
-         artistasIniciales:[],
-         isLoaded: false  //cuando se renderiza, al principio no hay ningún dato
+         artistasIniciales:[], //este artistas iniciales lo usamos porque cuando nosotros filtramos, si no tenemos el listado con los artistas iniciales, el componente padre se va a quedar solo con los elementos filtrados
+         isLoaded: false,  //cuando se renderiza, al principio no hay ningún dato
+         ordenarTarjetas: false,
         }
     }
 
@@ -23,7 +24,7 @@ class Artistas extends Component{
                 console.log(data);
                 this.setState({     //setState para que se actualicen los artistas
                     artistas:  data.data,
-                    artistasIniciales: data.data,
+                    artistasIniciales: data.data, //entonces para poder hacer lo de la linea 12 hay que decirle a la api que valor va a tener el array artistasIniciales (que va a ser todo el array que trae la api)
                     isLoaded: true
                 })   
             })
@@ -61,27 +62,59 @@ class Artistas extends Component{
     }
 
     deleteCard(artistasABorrar){
-        let artistasQueQuedan = this.state.artistasIniciales.filter( artista => artista.id !== artistasABorrar );
+        let artistasQueQuedan = this.state.artistasIniciales.filter( artista => artista.id !== artistasABorrar ); //filtro artistasIniciales porque es el array que se va a "pisar con el filtrado"
 
         this.setState({
-            artistas: artistasQueQuedan,
-            artistasIniciales: artistasQueQuedan
+            artistas: artistasQueQuedan, //aca actualizo personajes, para que de esa forma pueda ir de atras para adelante y viceversa
+            artistasIniciales: artistasQueQuedan //en este caso lo que hacemos es actualizar el array artistasIniciales con artistasQueQuedan porque cuando borramos un artista nos 
         })
     }
     
     filtrarArtistas(text){
-        let artistasFiltrados = this.state.artistasIniciales.filter( artista => artista.name.toLowerCase().includes(text.toLowerCase()));
+        let artistasFiltrados = this.state.artistasIniciales.filter( artista => artista.name.toLowerCase().includes(text.toLowerCase())); //personajes.name es la card //variable donde tengo solo los personajes que yo quiero //el metodo includes nos va a devolver un dato de tipo booleano (entonces si en el texto que yo filtro aparece algun artista, el includes me debe devolver un true si, no esta un false) y le ponemos un toLowercase para que los datos nos los traiga todo en minuscula osea para homogenizar el formato de la info
         this.setState({
             artistas: artistasFiltrados  
         })
        
     }
+    
+    ordenarTarjetas(){
+       let ordenarArtistas = this.state.artistas;
+        if(this.state.ordenarTarjetas){
+            let or = []
+          let ordenAsc = ordenarArtistas.map((artista)=>(artista.name ))
+           ordenAsc.sort()
+           ordenAsc.map( (orden)=>(
+            or = or.concat(this.state.artistas.filter( artista => artista.name.includes(orden)))
+           ))
+           console.log(or)
+           this.setState({
+            artistas:or,
+            ordenarTarjetas:false,
+           })
+        }
+        else{
+            let or = []
+          let ordenAsc = ordenarArtistas.map((artista)=>(artista.name ))
+           ordenAsc.sort()
+           ordenAsc.reverse()
+           ordenAsc.map( (orden)=>(
+            or = or.concat(this.state.artistas.filter( artista => artista.name.includes(orden)))
+           ))
+           console.log(or)
+           this.setState({
+            artistas:or,
+            ordenarTarjetas:true,
+           })
+        }
+     }
+     //en la linea 95 lo que hacemos con el filtrarArtistas es pasar la funcion como prop para poder usarlo en el componente "hijo" que en este caso seria el header
 
     render(){
         return(
             <React.Fragment>
                 <div className="row header">
-                    <Header filtrarArtistas={(text)=>this.filtrarArtistas(text)} botonRow={(botonRow) => this.botonRow(botonRow)}  boton={this.state.botonRow}/>
+                    <Header filtrarArtistas={(text)=>this.filtrarArtistas(text)} botonRow={(botonRow) => this.botonRow(botonRow)}  boton={this.state.botonRow} ordenarTarjetas={() => this.ordenarTarjetas()}  flechas={this.state.ordenarTarjetas}/>
                 </div>
 
                 <div className="row card-container">
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ class Header extends Component{
     constructor(){
         super();
         this.state ={
-            filterBy:'',
+            filterBy:'', //este filterBy va como un array vacio y despues va a ser modificado en la linea 21 y 22 cuando el usuario le mande datos al imput
             }
         }
 
@@ -18,19 +18,21 @@ class Header extends Component{
 
     controlarCambios(event){
         this.setState({
-            filterBy: event.target.value
-        }, () => this.props.filtrarArtistas(this.state.filterBy))
-        
-    }
+            filterBy: event.target.value //este value de aca referencia al value del campo imput en la linea 36
+        }, () => this.props.filtrarArtistas(this.state.filterBy)) //ale en la clase mostro como con un console log dentro del setState se qudaba un paso atras en la ejecucion del texto // this.state despues de llamar a set state puede devolver un valor inexacto. react nos permite poner despues del objt literal un callback que nos garantiza la ejecucion una vez que la actualizacion de estado se haya completado
+               
+    }                                                             //this.state.filterBy va a ser el que tiene la info a ejecutarse
 
     render(){  
         return( 
             <section>
                 <img src='../assets/img/eMusic.png' alt=""/>
-                <p className="ordenar">Ordenar ASC/ DESC</p>
+                {this.props.flechas ?
+                 <p className="ordenar" onClick = {()=>this.props.ordenarTarjetas()}>Ordenar asc</p> 
+                 :<p className="ordenar" onClick = {()=>this.props.ordenarTarjetas()}>Ordenar desc</p>}
                 <i className="fas fa-align-justify" onClick={()=>this.props.botonRow(this.props.boton)}></i>
                 <i className="fas fa-th" onClick={()=>this.props.botonRow(this.props.boton)}></i>
-                <form action="Buscar por Nombre" onSubmit={(submit)=>this.evitarSubmit(submit)}>
+                <form action="Buscar por Nombre" onSubmit={(submit)=>this.evitarSubmit(submit)}> 
                 <input type="text" onChange={(filtrado)=>this.controlarCambios(filtrado)} value={this.state.filterBy} placehoder='ingrese su nombre'/>
                 </form>
             </section>    
@@ -39,3 +41,5 @@ class Header extends Component{
 }
 
 export default Header;
+// este form (linea 35) es igual al que veniamos haciendo pero con la particularidad de que en este le debemos pasar como parametro el evento(submit)
+//en la linea 36, definimos el evento "onChange" para que el estado del componente se actualize automaticamente cuando usuario cambia o agrega algun caracter y se guarda en value que es el que modifca el estado event.target.value en la linea 21
